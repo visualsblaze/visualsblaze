02931c138651b54abf6fa8ada9a0194f02218417
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2025-01-13 01:12:28 +0500

    Add

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import abimg from '../../images/about.jpg';
 import { Link } from 'react-router-dom';
-import photoshope from "../../images/photoshope img.png";
-import premiere from "../../images/premiere img.png";
+// import photoshope from "../../images/photoshope img.png";
+// import premiere from "../../images/premiere img.png";
 import xd from "../../images/xd img.png";
 import effects from "../../images/after effects img.png";
-import indesign from "../../images/indesign img.png";
-import media from "../../images/media endcoder img.png";
+// import indesign from "../../images/indesign img.png";
+// import media from "../../images/media endcoder img.png";
 import figma from "../../images/figma img.png";
 import illus from "../../images/illustrator img.png";
-import choose from "../../images/choos.png";
-import fact from "../../images/fact.png";
+import choose from "../../images/choose-us-img.png";
+import gball from "../../images/golden ball img.png";
+import wball from "../../images/white ball img.png";
+import fact1 from "../../images/revenue img.png";
+import fact2 from "../../images/consumers img.png";
+import fact3 from "../../images/majority img.png";
+import fact4 from "../../images/managers img.png";
+
+
+
+
+
 
 
 
@@ -63,7 +73,7 @@ const About = (props) => {
                     </div>
                 </div>
 
-               {/* Why Choose Us Section */}
+   {/* Why Choose Us Section */}
 <section className="why-choose-us-section">
     <div className="container">
         <div className="row align-items-center">
@@ -72,91 +82,155 @@ const About = (props) => {
                 <div className="why-choose-us-image">
                     <img src={choose} alt="Why Choose Us Graphic" />
                 </div>
-                </div>
-
-            
-        </div>
-    </div>
-</section>
-
-                <section className="tools-section">
-    <div className="container">
-        <h2 className="section-title">Expertise in Modern</h2>
-        <p className="tools-description">
-        Graphic Design Applications
-        </p>
-        <div className="tools-list">
-            <div className="tool-item">
-                <img src={photoshope} alt="Photoshop" />
             </div>
-            <div className="tool-item">
-                <img src={premiere} alt="premiere" />
-            </div>
-            <div className="tool-item">
-                <img src={xd} alt="xd" />
-            </div>
-            <div className="tool-item">
-                <img src={effects} alt="After Effects" />
-            </div>
-            <div className="tool-item">
-                <img src={indesign} alt="indesgin" />
-            </div>
-            <div className="tool-item">
-                <img src={media} alt="media" />
-            </div>
-            <div className="tool-item">
-                <img src={figma} alt="figma" />
-            </div>
-            <div className="tool-item">
-                <img src={illus} alt="illus" />
-            </div>
-        </div>
-    </div>
-</section>
-<section className="why-choose-us-section">
-    <div className="container">
-        <div className="row align-items-center">
             <div className="col-lg-6">
-                {/* Image Section */}
-                <div className="why-choose-us-image">
-                    <img src={fact} alt="Why Choose Us Graphic" />
-                </div>
-                </div>
+                {/* Text Section */}
+                <div className="why-choose-us-text">
+                    <p>
+                    When it comes to graphic design, choosing the right partner can make all the difference in the success of your brand. At our studio, we offer more than just design services, we provide a comprehensive solution tailored to your unique needs. Our team is comprised of skilled professionals who are passionate about creativity and innovation. We understand that effective graphic design goes beyond aesthetics; it's about communicating your message clearly and engaging your audience on a deeper level. With our expertise in both traditional and digital design techniques, we can bring your vision to life across various platforms and mediums. What sets us apart is our commitment to excellence, attention to detail, and dedication to exceeding your expectations. When you choose us for your graphic design needs, you're not just getting a service provider, you're gaining a strategic partner invested in your success.
+                    </p>
+                    <br/>
+                    <br/>
 
-            
+                    <h2 className="section-title"> Why Choose Us</h2>
+                  
+                </div>
+            </div>
         </div>
     </div>
 </section>
-                {/* Quick Fun Fact Section
-                <div className="row align-items-center">
-                    <div className="col-lg-12">
-                        <div className="quick-fun-fact">
-                            <h2>Quick Fun Fact!</h2>
-                            <div className="facts">
-                                <div className="fact">
-                                    <h3>23%</h3>
-                                    <p>The typical revenue boost linked to consistently maintaining a brand’s presentation.</p>
-                                    <span>Source: Lucidpress</span>
-                                </div>
-                                <div className="fact">
-                                    <h3>86%</h3>
-                                    <p>Most consumers are willing to pay a premium for brands they recognize and trust.</p>
-                                    <span>Source: Superoffice.com</span>
-                                </div>
-                                <div className="fact">
-                                    <h3>71%</h3>
-                                    <p>The majority of B2B marketing leaders emphasize branding for growth.</p>
-                                    <span>Source: Circle Research</span>
-                                </div>
-                                <div className="fact">
-                                    <h3>Over 70%</h3>
-                                    <p>Brand managers emphasize that cultivating an audience holds long-term value.</p>
-                                    <span>Source: Onbrand.me</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
+<section className="tools-section">
+  <div className="container">
+    <h2 className="section-title">Expertise in Modern</h2>
+    <p className="tools-description">Graphic Design Applications</p>
+    <br/>
+
+    <div className="tools-list">
+      <div className="tool-item">
+        <img className="tool-icon" src={xd} alt="XD" />
+        <img className="ball white-ball" src={wball} alt="White Ball" />
+        <img className="ball gold-ball" src={gball} alt="Gold Ball" />
+      </div>
+      <div className="tool-item">
+        <img className="tool-icon" src={effects} alt="After Effects" />
+        <img className="ball white-ball-second" src={wball} alt="White Ball" />
+        <img className="ball gold-ball-second" src={gball} alt="Gold Ball" />
+      </div>
+      <div className="tool-item">
+        <img className="tool-icon" src={figma} alt="Figma" />
+        <img className="ball white-ball" src={wball} alt="White Ball" />
+        <img className="ball gold-ball" src={gball} alt="Gold Ball" />
+      </div>
+      <div className="tool-item">
+        <img className="tool-icon" src={illus} alt="Illustrator" />
+        <img className="ball white-ball-second" src={wball} alt="White Ball" />
+        <img className="ball gold-ball-second" src={gball} alt="Gold Ball" />
+      </div>
+      <div className="tool-item">
+        <img className="tool-icon" src={illus} alt="Illustrator" />
+        <img className="ball white-ball" src={wball} alt="White Ball" />
+        <img className="ball gold-ball" src={gball} alt="Gold Ball" />
+      </div><div className="tool-item">
+        <img className="tool-icon" src={illus} alt="Illustrator" />
+        <img className="ball white-ball-second" src={wball} alt="White Ball" />
+        <img className="ball gold-ball-second" src={gball} alt="Gold Ball" />
+      </div><div className="tool-item">
+        <img className="tool-icon" src={illus} alt="Illustrator" />
+        <img className="ball white-ball" src={wball} alt="White Ball" />
+        <img className="ball gold-ball" src={gball} alt="Gold Ball" />
+      </div><div className="tool-item">
+        <img className="tool-icon" src={illus} alt="Illustrator" />
+        <img className="ball white-ball-second" src={wball} alt="White Ball" />
+        <img className="ball gold-ball-second" src={gball} alt="Gold Ball" />
+      </div>
+    </div>
+  </div>
+</section>
+
+<section className="quick-fun-fact-section">
+  <div className="container">
+    <h2 className="section-titles"><span className="quick-funs">Quick Fun</span> <br />
+    <span className="facts">Fact</span></h2>
+    <div className="row">
+      <div className="col-lg-6">
+      <div className="fun-fact-item">
+  <div className="fact-content">
+    <img src={fact1} alt="Fact Icon" className="fact-image" />
+    <p className="fact-text">
+  <h3 >+23%</h3>
+
+      The typical revenue boost linked to consistently maintaining a brand's presentation.
+      <br />
+      <span className="fact-source">Source: Lucidpress</span>
+    </p>
+  </div>
+  </div>
+
+ 
+
+      </div>
+      <div className="col-lg-6">
+      <div className="fun-fact-item">
+  <div className="fact-content">
+    <img src={fact2} alt="Fact Icon" className="fact-image" />
+    <p className="fact-text">
+  <h3 >+86%</h3>
+
+  most consumers are willing to pay a 
+premium for brands they recognize and 
+trust, prioritizing a superior customer 
+experience over both price and product 
+quality.
+      <span className="fact-source">Source: Superoff ice.com</span>
+    </p>
+  </div>
+
+</div>
+
+      </div>
+      <div className="col-lg-6">
+      <div className="fun-fact-item">
+  <div className="fact-content">
+    <img src={fact3} alt="Fact Icon" className="fact-image" />
+    <p className="fact-text">
+  <h3 >77%</h3>
+
+  the majority of B2B marketing leaders 
+emphasize the importance of branding 
+for achieving growth.
+      <br />
+      <span className="fact-source">Source: Circle Research</span>
+    </p>
+  </div>
+
+</div>
+
+      </div>
+      <div className="col-lg-6">
+      <div className="fun-fact-item">
+  <div className="fact-content">
+    <img src={fact4} alt="Fact Icon" className="fact-image" />
+
+    <p className="fact-text">
+  <h3 >OVER 70%</h3>
+
+  brand managers worldwide emphasize 
+that cultivating an audience holds 
+greater long-term value than immediate 
+direct sales.
+      <br />
+      <span className="fact-source">Source: Onbrand.me</span>
+    </p>
+  </div>
+</div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+
+               
             </div>
         </section>
     )
