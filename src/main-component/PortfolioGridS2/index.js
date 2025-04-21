22a0a4c82675bc8254dd752394aff0fe43413c0c
Hamza Manzoor <77552469+HH-pro@users.noreply.github.com>
2025-04-22 01:18:48 +0500

    Initial

--- src/main-component/PortfolioGridS2/index.js
+++ src/main-component/PortfolioGridS2/index.js
@@ -25,7 +25,7 @@ import project13 from '../../images/dental-post-cover-image.jpg';
 import project14 from '../../images/dental-post-inner-image.jpg';
 import project15 from '../../images/ahmed-mohammed-co-cover-image.jpg';
 import project16 from '../../images/ahmed-mohammed-co-inner-image.jpg';
-import project17 from '../../images/sfd-offers-cover-image.jpg';
+import project17 from '../../images/sfd-offers-cover-image.png';
 import project18 from '../../images/sfd-offers-inner-image-design.jpg';
 import project19 from '../../images/sfd-flyer-cover-image.jpg';
 import project20 from '../../images/sfd-flyer-inner-image.jpg';
